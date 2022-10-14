c1b99e68ad70a347b7ff2965d55b77a47bf64c86
IgorYudintsev <kakvospitat@gmail.com>
2022-10-14 23:12:32 +0300

    10. Уроки React JS (default опасность плагин auto import)

---
+++ src/Technologies.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+export const Technologies = () => {
+    return (
+    <ul>
+        <li>HTML</li>
+        <li>CSS</li>
+        <li>TS</li>
+        <li>REACT</li>
+    </ul>
+    );
+};
+
