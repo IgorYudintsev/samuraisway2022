d2b65702f5e2f6ab1651f9a7bfa033aa270da85a
IgorYudintsev <kakvospitat@gmail.com>
2022-10-29 18:16:46 +0300

    14. Уроки React JS (css-модули, css-modules)-STYLED COMPONENTS

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,11 +1,22 @@
 import React from 'react';
+import styled from "styled-components";
 
 export const Header = () => {
     return (
         <div className={'Header'}>
+            <ImgWrapper>
+                <img src="https://upload.wikimedia.org/wikipedia/commons/d/db/Zeronet_logo.png"
+                     alt="logo"/>
+            </ImgWrapper>
 
-            <img className={'Logo'} src="https://upload.wikimedia.org/wikipedia/commons/d/db/Zeronet_logo.png" alt="logo"/>
         </div>
     );
 };
+
+const ImgWrapper = styled.span`
+  &>img{
+    width: 50px;
+  }
+
+`
 
