edb4e89231d866c9d33cf908384885d806efdb0c
IgorYudintsev <kakvospitat@gmail.com>
2022-12-02 20:25:05 +0300

    18. Уроки React JS (страница Dialogs) - react курсы бесплатно

--- src/App.tsx
+++ src/App.tsx
@@ -4,13 +4,15 @@ import {Header} from "./components/Header/Header";
 import {Footer} from "./Footer";
 import {Profile} from "./components/Profile/Profile";
 import {Navbar} from "./components/Navbar/Navbar";
+import {Dialogs} from "./components/Dialogs/Dialogs";
 
 function App() {
     return (
         <div className="AppWrapper">
             <Header/>
             <Navbar/>
-            <Profile/>
+            {/*<Profile/>*/}
+            <Dialogs/>
             <Footer/>
         </div>
     );
