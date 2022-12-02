61fc9b5b563f92e46159c56ab7242685100e123d
IgorYudintsev <kakvospitat@gmail.com>
2022-12-03 01:18:45 +0300

    20. Уроки React JS (NavLink) - react курсы бесплатно

--- src/components/Navbar/Navbar.tsx
+++ src/components/Navbar/Navbar.tsx
@@ -1,15 +1,39 @@
 import React from 'react';
 import {NavLink} from 'react-router-dom';
+import styled from "styled-components";
 
 export const Navbar = () => {
     return (
         <div className={'Nav'}>
-            <div><NavLink to={'/profile'}> Profile</NavLink></div>
-            <div><NavLink to={'/dialogs'}> Dialogs</NavLink></div>
-            <div><NavLink to={'/messages'}> Messages</NavLink></div>
-            <div><NavLink to={'/news'}>News</NavLink></div>
-            <div><NavLink to={'/music'}>Music</NavLink></div>
-            <div><NavLink to={'/seetings'}>Seetings</NavLink></div>
+            <NavWrapper><NavLink to={'/profile'}> Profile</NavLink></NavWrapper>
+            <NavWrapper><NavLink to={'/dialogs'}> Dialogs</NavLink></NavWrapper>
+            <NavWrapper><NavLink to={'/messages'}> Messages</NavLink></NavWrapper>
+            <NavWrapper><NavLink to={'/news'}>News</NavLink></NavWrapper>
+            <NavWrapper><NavLink to={'/music'}>Music</NavLink></NavWrapper>
+            <NavWrapper><NavLink to={'/seetings'}>Seetings</NavLink></NavWrapper>
         </div>
     );
 };
+
+
+const NavWrapper = styled.div`
+  margin-left: 10px;
+  font-size: 20px;
+  & > a {
+    text-decoration: none;
+    color: white;
+  }
+  & > a.active {
+    text-decoration: none;
+    color: black;
+  }
+  
+  & > a:hover {
+    //background: #786b59; /* Цвет фона под ссылкой */
+    color: steelblue; /* Цвет ссылки */
+  }
+
+  //a:visited {
+  //  color: #b4ddf6;
+  //}
+`
