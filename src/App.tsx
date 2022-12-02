a18f941df8296d8ac77164db903a64ffc567c115
IgorYudintsev <kakvospitat@gmail.com>
2022-12-03 00:33:36 +0300

    19. Уроки React JS (route, browser-router, маршрутизация) - react курсы бесплатно

--- src/App.tsx
+++ src/App.tsx
@@ -5,14 +5,20 @@ import {Footer} from "./Footer";
 import {Profile} from "./components/Profile/Profile";
 import {Navbar} from "./components/Navbar/Navbar";
 import {Dialogs} from "./components/Dialogs/Dialogs";
+import {Route, Routes } from 'react-router-dom';
 
 function App() {
     return (
         <div className="AppWrapper">
             <Header/>
             <Navbar/>
-            {/*<Profile/>*/}
-            <Dialogs/>
+            <Routes>
+              <Route path={'/profile'} element={ <Profile/>}/>
+              <Route path={'/dialogs'} element={  <Dialogs/>}/>
+
+            </Routes>
+
+
             <Footer/>
         </div>
     );
--- src/components/Navbar/Navbar.tsx
+++ src/components/Navbar/Navbar.tsx
@@ -1,10 +1,15 @@
 import React from 'react';
+import {NavLink} from 'react-router-dom';
 
 export const Navbar = () => {
     return (
         <div className={'Nav'}>
-            <div>Messages</div>
-            <div>Main content</div>
+            <div><NavLink to={'/profile'}> Profile</NavLink></div>
+            <div><NavLink to={'/dialogs'}> Dialogs</NavLink></div>
+            <div><NavLink to={'/messages'}> Messages</NavLink></div>
+            <div><NavLink to={'/news'}>News</NavLink></div>
+            <div><NavLink to={'/music'}>Music</NavLink></div>
+            <div><NavLink to={'/seetings'}>Seetings</NavLink></div>
         </div>
     );
 };
