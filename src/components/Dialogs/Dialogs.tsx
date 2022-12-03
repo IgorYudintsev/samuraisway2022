2176948da831c90dfdbb52e939e49f8c432bd08b
IgorYudintsev <kakvospitat@gmail.com>
2022-12-03 11:13:30 +0300

    21. Уроки React JS (Верстаем страницу Диалогов) - react курсы бесплатно

--- src/components/Dialogs/Dialogs.tsx
+++ src/components/Dialogs/Dialogs.tsx
@@ -1,10 +1,35 @@
 import React from 'react';
+import styled from "styled-components";
 
 export const Dialogs = () => {
     return (
-        <div>
-            Dialogs
-        </div>
+        <Wrapper>
+            <Left>
+                <div>Igor</div>
+                <div>Olga</div>
+                <div>Sasha</div>
+            </Left>
+            <Right>
+                <div>Hi</div>
+                <div>How are you&</div>
+                <div>hellow</div>
+            </Right>
+        </Wrapper>
     );
 };
 
+const Wrapper = styled.div`
+  display: flex;
+  width: 100%;
+
+`
+
+const Left = styled.div`
+  background-color: steelblue;
+  width: 50%;
+`
+
+const Right = styled.div`
+  background-color: #1e3786;
+  width: 50%;
+`
