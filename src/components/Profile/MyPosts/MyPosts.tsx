89d653825cb3679695e80652d9c3be28a07b5bf6
IgorYudintsev <kakvospitat@gmail.com>
2023-04-22 18:56:54 +0300

    87 - shouldComponentUpdate, PureComponent, memo - React JS

--- src/components/Profile/MyPosts/MyPosts.tsx
+++ src/components/Profile/MyPosts/MyPosts.tsx
@@ -1,8 +1,7 @@
-import React, {useRef} from 'react';
+import React from 'react';
 import styled from "styled-components";
 import {PostType} from "../../../redux/store";
 import {Post} from "./Post/Post";
-import {SubmitHandler, useForm} from "react-hook-form";
 import {CommonForm} from "../../common/CommonForm";
 
 
@@ -10,73 +9,89 @@ type PostsType = {
     posts: PostType[]
     newPostText: string
     //onPostChange: (newText: string) => void
-    addPost: (message:string) => void
+    addPost: (message: string) => void
 }
 
 
-export const MyPosts = (props: PostsType) => {
-    const posts = props.posts.map(post => {
-        return (
-            <Post message={post.message} likesCount={post.likesCount}/>
-        )
-    })
-
-
-    const onSubmitHandler = (message: string) => {
-             props.addPost(message)
+export class MyPosts extends React.PureComponent<PostsType> {
+    shouldComponentUpdate(nextProps: Readonly<PostsType>, nextState: Readonly<{}>): boolean {
+        return nextProps!==this.props || nextState!==this.state
     }
 
+    render() {
+        const posts = this.props.posts.map(post => {
+            return (
+                <Post message={post.message} likesCount={post.likesCount}/>
+            )
+        })
 
 
-    return (
-        <MyPostsWrapper>
-            <div>MyPosts</div>
-            {/*<textarea cols={30} rows={10} ref={newPostElement} value={props.newPostText} onChange={onChangeHandler}/>*/}
-            {/*<div>*/}
-            {/*    <button onClick={addPostHandler}>add post</button>*/}
-            {/*    <button>remove post</button>*/}
-            {/*</div>*/}
+        const onSubmitHandler = (message: string) => {
+            this.props.addPost(message)
+        }
 
-            {/*<AddMessageFormMyPosts onSubmitHandler={onSubmitHandler}/>*/}
-            <CommonForm onSubmitHandler={onSubmitHandler} textArea={true}/>
 
-            {posts}
-        </MyPostsWrapper>
-    );
-};
+        return (
+            <MyPostsWrapper>
+                <div>MyPosts</div>
+
+                <CommonForm onSubmitHandler={onSubmitHandler} textArea={true}/>
+
+                {posts}
+            </MyPostsWrapper>
+        );
+    }
+}
 
 const MyPostsWrapper = styled.div`
   margin-left: 20px;
 `
 
+//--------------------------------------------------------------------------------
 
-
-
+// import React from 'react';
+// import styled from "styled-components";
+// import {PostType} from "../../../redux/store";
+// import {Post} from "./Post/Post";
+// import {CommonForm} from "../../common/CommonForm";
 //
 //
-// type Inputs = {
-//     message: string,
-// };
-//
-// type AddMessageFormMyPostsType = {
-//     onSubmitHandler: (message: string) => void
+// type PostsType = {
+//     posts: PostType[]
+//     newPostText: string
+//     //onPostChange: (newText: string) => void
+//     addPost: (message:string) => void
 // }
 //
-// export const AddMessageFormMyPosts = (props: AddMessageFormMyPostsType) => {
-//     const {register, handleSubmit, reset} = useForm<Inputs>();
-//     const onSubmit: SubmitHandler<Inputs> = (data) => {
-//         props.onSubmitHandler(data.message)
-//         reset()
+//
+// export const MyPosts = (props: PostsType) => {
+//     const posts = props.posts.map(post => {
+//         return (
+//             <Post message={post.message} likesCount={post.likesCount}/>
+//         )
+//     })
+//
+//
+//     const onSubmitHandler = (message: string) => {
+//         props.addPost(message)
 //     }
 //
 //
-//     return (
-//         <form onSubmit={handleSubmit(onSubmit)}>
-//             <div>
-//                 <textarea cols={30} rows={10}  placeholder={'Enter your message'} {...register("message",{required: true})} />
-//             </div>
-//             <input type="submit"/>
-//         </form>
 //
-//     )
-// }
+//     return (
+//         <MyPostsWrapper>
+//             <div>MyPosts</div>
+//
+//             <CommonForm onSubmitHandler={onSubmitHandler} textArea={true}/>
+//
+//             {posts}
+//         </MyPostsWrapper>
+//     );
+// };
+//
+// const MyPostsWrapper = styled.div`
+//   margin-left: 20px;
+// `
+
+
+
