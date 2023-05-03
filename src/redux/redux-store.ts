0a9ec3a059b98374183b0b4e93dc8d703502e981
IgorYudintsev <kakvospitat@gmail.com>
2023-05-03 23:49:49 +0300

    91 - chrome extensions для react и redux - React JS

--- src/redux/redux-store.ts
+++ src/redux/redux-store.ts
@@ -1,4 +1,4 @@
-import {AnyAction, applyMiddleware, combineReducers, legacy_createStore as createStore} from "redux";
+import {AnyAction, applyMiddleware, combineReducers, compose, legacy_createStore as createStore} from "redux";
 import {DialogsReducer} from "./dialogs-reducer";
 import {ProfileReducer} from "./profile-reducer";
 import {InitialStateType, UsersReducer} from "./users-reducer";
@@ -6,25 +6,29 @@ import {AuthReducer} from "./auth-reducer";
 import thunk, {ThunkAction, ThunkDispatch} from "redux-thunk";
 import {AppReducer} from "./app-reducer";
 
-let reducers=combineReducers({
+let reducers = combineReducers({
     dialogsPage: DialogsReducer,
     profilePage: ProfileReducer,
-    usersPage:UsersReducer,
-    auth:AuthReducer,
+    usersPage: UsersReducer,
+    auth: AuthReducer,
     app: AppReducer
 })
-export type reducersType=ReturnType<typeof reducers>
-export type storeType=typeof store
+export type reducersType = ReturnType<typeof reducers>
+export type storeType = typeof store
 
-export let store=createStore(reducers,applyMiddleware(thunk))
-export type AppDispatch = ThunkDispatch<RootState, unknown,AnyAction>
-export type RootState = ReturnType<typeof store.getState>
-export type AppThunk<ReturnType = void> = ThunkAction<
-    ReturnType,
-    RootState,
-    unknown,
-    AnyAction
-    >
+//export let store = createStore(reducers, applyMiddleware(thunk))
 
 // @ts-ignore
-window.store=store
+const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
+export const store = createStore(reducers, composeEnhancers(applyMiddleware(thunk)));
+
+export type AppDispatch = ThunkDispatch<RootState, unknown, AnyAction>
+export type RootState = ReturnType<typeof store.getState>
+export type AppThunk<ReturnType = void> = ThunkAction<ReturnType,
+    RootState,
+    unknown,
+    AnyAction>
+
+// @ts-ignore
+window.store = store
+export default store
